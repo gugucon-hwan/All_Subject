84f3347477d9b6b9e17f14d460eed559e6f5c8b0
gugucon-hwan <gugucon2018@gmail.com>
2020-09-26 19:23:41 +0900

    리액트 진도 나감 (자바스크립트 진행 중)

---
+++ StudyReact/dev/router-tutorial/src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Route, Link, Switch } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profiles/gildong">gildong 프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path={["/about", "/info"]} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          //path를 따로 정의하지 않으면 모든 상황에 렌더링됨
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다.</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
+    </div>
+  );
+};
+
+export default App;
