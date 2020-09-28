ac7608bedfa965ab346e0d1ddd59fc65100a082b
gugucon-hwan <gugucon2018@gmail.com>
2020-09-28 17:54:06 +0900

    리액트 진도 나감

---
+++ StudyReact/dev/context-tutorial/src/components/ColorBox.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import ColorContext from "../contexts/color";
+
+const ColorBox = () => {
+  const { state } = useContext(ColorContext);
+  return (
+    <>
+      {({ state }) => (
+        <>
+          <div
+            style={{
+              width: "64px",
+              height: "64px",
+              background: state.color,
+            }}
+          />
+          <div
+            style={{
+              width: "32px",
+              height: "32px",
+              background: state.subColor,
+            }}
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default ColorBox;
--- StudyReact/dev/news-viewer/src/components/NewsList.js
+++ StudyReact/dev/news-viewer/src/components/NewsList.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 import React, { useEffect, useState } from 'react';
 
 const NewsListBlock = styled.div`
@@ -24,37 +25,29 @@ const sampleArticle = {
 };
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    //async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=1c4548a2d5634365a35403fb82ba6ff6`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, [category]);
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=1c4548a2d5634365a35403fb82ba6ff6`,
+    );
+  });
 
   //대기 중일 때
   if (loading) {
     return <NewsListBlock>대기 중...</NewsListBlock>;
   }
   //아직 articles 값이 설정되지 않았을 때
-  if (!articles) {
+  if (!response) {
     return null;
   }
 
+  //에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생</NewsListBlock>;
+  }
+
   //articles값이 유효할 때
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
