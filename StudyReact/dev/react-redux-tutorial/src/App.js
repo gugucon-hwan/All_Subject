0200b2008035b1d56b0053a4bd504949b64d27ca
gugucon-hwan <gugucon2018@gmail.com>
2020-09-29 23:17:40 +0900

    리액트 진도나감

---
+++ StudyReact/dev/react-redux-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './components/Counter.js';
+
+const App = () => {
+  return (
+    <div>
+      <Counter number={0} />
+    </div>
+  );
+};
+
+export default App;
