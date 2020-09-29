98200cef8c7438e83dabee062560c739abf3d4db
gugucon-hwan <gugucon2018@gmail.com>
2020-09-30 03:00:44 +0900

    스프링 웹프로젝트 진도나감

--- Spring_WebProject/ex00/src/main/java/org/zerock/sample/SampleHotel.java
+++ Spring_WebProject/ex00/src/main/java/org/zerock/sample/SampleHotel.java
@@ -2,16 +2,19 @@ package org.zerock.sample;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Component
 @ToString
 @Getter
+@RequiredArgsConstructor
 public class SampleHotel {
-	 private Chef chef;
+
+	@NonNull
+	private Chef chef;
 	 
-	 public SampleHotel(Chef chef) {
-		 this.chef=chef;
-	 }
 }
