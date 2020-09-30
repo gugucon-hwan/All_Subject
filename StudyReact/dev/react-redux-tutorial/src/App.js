4fdd7dc2fceb9ea5f8c576557ba3f58f2a375a80
gugucon-hwan <gugucon2018@gmail.com>
2020-09-30 14:05:15 +0900

    리액트 진도 나감

--- StudyReact/dev/react-redux-tutorial/src/App.js
+++ StudyReact/dev/react-redux-tutorial/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import Counter from './components/Counter.js';
+//import Counter from './components/Counter.js';
+import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 
 const App = () => {
   return (
     <div>
-      <Counter number={0} />
+      <CounterContainer />
+      <hr />
+      <TodosContainer />
     </div>
   );
 };
---
+++ StudyReact/dev/react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Counter from '../components/Counter';
+import { connect } from 'react-redux';
+import { increase, decrease } from '../modules/counter';
+import { bindActionCreators } from 'redux';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+/*
+const mapStateToProps = (state) => ({
+  number: state.counter.number,
+});
+
+const mapDispatchToPros = (dispatch) => ({
+  //임시 함수
+  increase: () => {
+    dispatch(increase());
+  },
+  decrease: () => {
+    dispatch(decrease());
+  },
+});*/
+
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  {
+    increase,
+    decrease,
+  },
+)(CounterContainer);
