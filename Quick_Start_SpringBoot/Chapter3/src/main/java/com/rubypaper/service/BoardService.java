e8738bd63a7b406b1a62fe381d6fd8f707bc3355
gugucon-hwan <gugucon2018@gmail.com>
2020-10-01 23:42:14 +0900

    스프링부트 진도 나감

---
+++ Quick_Start_SpringBoot/Chapter3/src/main/java/com/rubypaper/service/BoardService.java
@@ -0,0 +1,13 @@
+package com.rubypaper.service;
+
+import java.util.List;
+
+import com.rubypaper.domain.BoardVO;
+
+public interface BoardService {
+	String hello(String name);
+	
+	BoardVO getBoard();
+	
+	List<BoardVO> getBoardList();
+}
