d73224bb49c70ceda4a0fa40f7031278f03f790b
gugucon-hwan <gugucon2018@gmail.com>
2020-10-02 22:19:11 +0900

    리액트 진도 나감

--- StudyReact/dev/learn-redux-middleware/src/modules/sample.js
+++ StudyReact/dev/learn-redux-middleware/src/modules/sample.js
@@ -1,22 +1,31 @@
-import { handleActions } from "redux-actions";
+import { createAction, handleActions } from "redux-actions";
+import { call, put, takeLatest } from "redux-saga/effects";
 import * as api from "../lib/api";
-import createRequestThunk from "../lib/createRequestThunk";
+import createReqeustSaga from "../lib/createRequestSaga";
 
 //액션 타입을 선언합니다.
 //한 요청당 세 개를 만들어야 한다.
 const GET_POST = "sample/GET_POST";
 const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
-//const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
+const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
 
 const GET_USERS = "sample/GET_USERS";
 const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
-//const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
+const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
 //thunk함수를 생성한다.
 //thunk함수 내부에서는 시작할 때, 성공할 때, 실패했을 때 다른 액션을 디스패치합니다.
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getPost = createAction(GET_POST, (id) => id);
+export const getUsers = createAction(GET_USERS);
+
+const getPostSaga = createReqeustSaga(GET_POST, api.getPost);
+const getUsersSaga = createReqeustSaga(GET_USERS, api.getUsers);
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
 
 //초기 상태를 선언합니다.
 //요청의 로딩 중 상태는 loading이라는 객체에서 관리한다.
