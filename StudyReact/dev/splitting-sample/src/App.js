f69713a299031ad9f7d7ab1202b45eb991af1656
gugucon-hwan <gugucon2018@gmail.com>
2020-10-03 20:58:40 +0900

    자바스크립트, 리액트 진도  나감

--- StudyReact/dev/splitting-sample/src/App.js
+++ StudyReact/dev/splitting-sample/src/App.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState, Suspense } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import notify from "./notify";
+import loadable from "@loadable/component";
 
-const App = () => {
+const SplitMe = React.lazy(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>,
+});
+
+function App() {
+  const [visible, setVisible] = useState(false);
   const onClick = () => {
-    import("./notify").then((result) => result.default());
+    setVisible(true);
+  };
+  const onMouseOVer = () => {
+    SplitMe.preload();
   };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}> Hello React!</p>
+        <p onClick={onClick} onMouseOver={onMouseOVer}>
+          Hello React!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
-};
+}
 
 export default App;
